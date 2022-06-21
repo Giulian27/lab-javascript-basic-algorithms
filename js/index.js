ec5f1f186344ec9c9f1051b07b352fb07c944e59
Giulian27 <106954301+Giulian27@users.noreply.github.com>
2022-06-21 21:44:41 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 // Iteration 1: Names and Input
-//
+
+let driver = "hacker1";
+console.log("The driver's name is " + driver);
+
+let driver2 = "hacker2";
+console.log("The navigator's name is " + driver2);
+
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
-
-console.log("I'm ready!");
